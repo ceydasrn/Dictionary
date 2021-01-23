b0010ea2e337abd9f99dab0b78acda23c3b41c8d
Ceyda Saran <cluise61@outlook.com>
2021-01-23 16:16:59 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+
+    class MyDictionary<T,T2>
+    {
+        T[] id;
+        T2[] names;
+
+        public MyDictionary()
+        {
+            id = new T[0];
+            names = new T2[0];
+        }
+        public void Add(T Id, T2 Name)
+        {
+            T[] tempArray = id;
+            T2[] tempArray2 = names;
+
+            id = new T[id.Length + 1];
+            names = new T2[names.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                id[i] = tempArray[i];
+                names[i] = tempArray2[i];
+                
+            }
+            id[id.Length - 1] = Id;
+            names[names.Length - 1] = Name;
+        }
+        public int Length
+        {
+            get { return id.Length; }
+
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> bilgiler = new MyDictionary<int, string>();
+            bilgiler.Add(1, "Ceyda");
+            Console.WriteLine(bilgiler.Length);
+            bilgiler.Add(2, "Baran");
+            Console.WriteLine(bilgiler.Length);
+        }
+    }
+}
